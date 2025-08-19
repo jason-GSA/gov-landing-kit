757f81ec024ae444353c3bb75c05717b0d9cdf8a
Jason Sun <54715047+jason-GSA@users.noreply.github.com>
2025-08-18 23:45:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/your-repo-name/' : '/',
+  base: mode === 'production' ? '/gov-landing-kit/' : '/',
   server: {
     host: "::",
     port: 8080,
